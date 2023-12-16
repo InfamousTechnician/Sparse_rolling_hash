5299bacc37bd7e7a3485404c00197d62176fcc13
Fekete Balazs Valer <48908633+InfamousTechnician@users.noreply.github.com>
2023-12-16 06:12:53 +0100

    Corrected dot-product calculation

--- sparse_rolling_hash.py
+++ sparse_rolling_hash.py
@@ -27,8 +27,10 @@ def KNN(vectors, K, v_in): # KNN search algo
     nearests = [None for _ in range(K)]
     distances = [maximum**2 for _ in range(K)]
     for vidx in range(len(vectors)):
+        distance = 0
         for idx in range(len(v_in)):
-            distance = (v_in[idx]**2 + vectors[vidx][idx]**2)**0.5
+            distance += v_in[idx] * vectors[vidx][idx]
+        distance **= 0.5
         for idx in range(K):
             if distance < distances[idx]:
                 nearests[idx] = vidx
@@ -36,30 +38,34 @@ def KNN(vectors, K, v_in): # KNN search algo
                 break
     return nearests
 
+K = 10 # looking so many nearest neighbours
+
 for _ in range(20): # running tests
 
-    # looking for K nearest neighbours in compressed format
-    K = 10
     starter = rdi(0, count)
     print(starter)
-    compressed_start = sparse_rolling_hash(sparse_vectors[starter], val_prime, dim_prime)
+    center = sparse_vectors[starter]
+    
+    # looking for K nearest neighbours in compressed format
+    compressed_start = sparse_rolling_hash(center, val_prime, dim_prime)
     compressed_near = KNN(compressed, K, compressed_start)
     print(compressed_near)
 
-    # testing for really nearest K neighbours of original dataset
-    compressed_sheer = KNN(sparse_vectors, K, sparse_vectors[starter])
-    print(compressed_sheer)
+    # testing for the really nearest K neighbours of the original dataset
+    sheer_near = KNN(sparse_vectors, K, center)
+    print(sheer_near)
 
     # comparing results by total distances
     total = 0
-    for compressed_near_ones in compressed_near:
+    for next_compressed_near_one in compressed_near:
         for idx in range(len(sparse_vectors[starter])):
-            total += (sparse_vectors[starter][idx]**2 + sparse_vectors[compressed_near_ones][idx]**2)**0.5 
+            total += (center[idx]**2 + sparse_vectors[next_compressed_near_one][idx]**2)**0.5 
     print(total)
 
     total = 0
-    for near_ones in compressed_sheer:
+    for next_near_one in sheer_near:
         for idx in range(len(sparse_vectors[starter])):
-            total += (sparse_vectors[starter][idx]**2 + sparse_vectors[near_ones][idx]**2)**0.5 
+            total += (center[idx]**2 + sparse_vectors[next_near_one][idx]**2)**0.5 
     print(total)
+    print()
 
